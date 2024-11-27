5a616e9c1be7f0f5adc83f79e08494922602e551
John <john-wang@hotmail.fr>
2024-11-27 23:12:59 +0100

    modif user input

--- episaine-front/src/components/customers/CreateCustomers.js
+++ episaine-front/src/components/customers/CreateCustomers.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import axios from "axios";
 import {
     TextField,
@@ -27,95 +27,99 @@ export default function Customers() {
     const [address, setAddress] = useState("");
     const [postal_code, setPostalCode] = useState("");
     const [errors, setErrors] = useState({});
+    const [touched, setTouched] = useState({});
 
-    const validateFields = () => {
+    const validateFields = useCallback(() => {
         const newErrors = {};
-        // Last Name
         if (!customer_lastname) {
             newErrors.customer_lastname = "Last Name is required";
-        }
-        else if (!/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/.test(customer_lastname)) {
+        } else if (!/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/.test(customer_lastname)) {
             newErrors.customer_lastname = "Last name must contain only letters.";
         }
-
-        // First Name
         if (!customer_firstname) {
             newErrors.customer_firstname = "First Name is required";
-        }
-        else if (!/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/.test(customer_firstname)) {
+        } else if (!/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/.test(customer_firstname)) {
             newErrors.customer_firstname = "First name must contain only letters.";
         }
-        // Birth Date
         if (!customer_birthdate) {
             newErrors.customer_birthdate = "Birth Date is required";
         }
-        // Gender
         if (!gender) {
-            newErrors.gender = "Gender is required"
+            newErrors.gender = "Gender is required";
         }
-        // Phone number
         if (!customer_phone_number) {
             newErrors.customer_phone_number = "Phone Number is required";
+        } else if (!/^(\+33|0)[1-9]\d{8}$/.test(customer_phone_number)) {
+            newErrors.customer_phone_number =
+                "Phone number not valid. Format : 0XXXXXXXXX";
         }
-        else if (!/^(\+33|0)[1-9]\d{8}$/.test(customer_phone_number)) {
-            newErrors.customer_phone_number = "Phone number not valid. Format : 0XXXXXXXXX";
-        }
-        // Email
         if (!customer_mail) {
             newErrors.customer_mail = "Email is required";
-        }
-        else if (!/\S+@\S+\.\S+/.test(customer_mail)) {
+        } else if (!/\S+@\S+\.\S+/.test(customer_mail)) {
             newErrors.customer_mail = "Email not valid.";
         }
-        // Postal code
         if (!postal_code) {
             newErrors.postal_code = "Postal Code is required";
-        }
-        else if (!/^\d{5}$/.test(postal_code)) {
+        } else if (!/^\d{5}$/.test(postal_code)) {
             newErrors.postal_code = "Postal code not valid.";
         }
-        // City
         if (!city) {
             newErrors.city = "City is required";
         }
-        // Address
         if (!address) {
             newErrors.address = "Address is required";
         }
-
         setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    }
+    }, [
+        customer_lastname,
+        customer_firstname,
+        customer_birthdate,
+        gender,
+        customer_phone_number,
+        customer_mail,
+        postal_code,
+        city,
+        address,
+    ]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (validateFields()) {
-            const customer = {
-                customer_lastname,
-                customer_firstname,
-                customer_birthdate,
-                gender,
-                customer_phone_number,
-                customer_mail,
-                city,
-                address,
-                postal_code,
-            }
-        axios.post(CREATE_CUSTOMER, customer)
-            .then(() => {
+        validateFields();
+        const customer = {
+            customer_lastname,
+            customer_firstname,
+            customer_birthdate,
+            gender,
+            customer_phone_number,
+            customer_mail,
+            city,
+            address,
+            postal_code,
+        };
+        setTouched(
+            Object.keys(customer).reduce((acc, field) => {
+                acc[field] = true;
+                return acc;
+            }, {})
+        );
+        if (Object.keys(errors).length === 0) {
+            axios.post(CREATE_CUSTOMER, customer).then(() => {
                 console.log("Submitted Data:", customer);
-            })
-            .catch((error) => {
+            }).catch((error) => {
                 console.error("Error while submitting data:", error);
             });
-        }
-        else {
-            console.error("Invalid data");
         }
     };
 
     const handleInputChange = (e, field) => {
         let value = e.target.value;
+    
+        // Supprimer les espaces pour le numéro de téléphone
+        if (field === "customer_phone_number") {
+            value = value.replace(/\s/g, '');
+        }
+    
+        // Mettre à jour la valeur et valider le champ correspondant
         switch (field) {
             case "customer_lastname":
                 setCustomerLastName(value);
@@ -124,7 +128,6 @@ export default function Customers() {
                 setCustomerFirstName(value);
                 break;
             case "customer_phone_number":
-                value = value.replace(/\s/g, '');
                 setPhoneNumber(value);
                 break;
             case "customer_mail":
@@ -148,11 +151,15 @@ export default function Customers() {
             default:
                 break;
         }
-
         validateFields();
     };
 
-    const isFormValid = Object.keys(errors).length === 0;
+    const handleBlur = (field) => {
+        setTouched((prev) => ({ ...prev, [field]: true }));
+        validateFields();
+    };
+
+    const isFormValid = Object.keys(errors).every((key) => !errors[key] || !touched[key]);
 
     return (
         <Container maxWidth="md" style={{ marginTop: "40px" }}>
@@ -171,8 +178,9 @@ export default function Customers() {
                             variant="outlined"
                             value={customer_lastname}
                             onChange={(e) => handleInputChange(e, "customer_lastname")}
-                            error={Boolean(errors.customer_lastname)}
-                            helperText={errors.customer_lastname}
+                            onBlur={() => handleBlur("customer_lastname")}
+                            error={Boolean(touched.customer_lastname && errors.customer_lastname)}
+                            helperText={touched.customer_lastname && errors.customer_lastname}
                         />
                         <TextField
                             fullWidth
@@ -181,19 +189,27 @@ export default function Customers() {
                             variant="outlined"
                             value={customer_firstname}
                             onChange={(e) => handleInputChange(e, "customer_firstname")}
-                            error={Boolean(errors.customer_firstname)}
-                            helperText={errors.customer_firstname}
+                            onBlur={() => handleBlur("customer_firstname")}
+                            error={Boolean(touched.customer_firstname && errors.customer_firstname)}
+                            helperText={touched.customer_firstname && errors.customer_firstname}
                         />
                         <LocalizationProvider dateAdapter={AdapterDateFns}>
                             <DatePicker
                                 label="Birth Date"
                                 value={customer_birthdate}
-                                onChange={(newValue) => handleInputChange({target : {value : newValue}}, "customer_birthdate")}
-                                error={Boolean(errors.customer_birthdate)}
-                                helperText={errors.customer_birthdate}
+                                onChange={(newValue) => handleInputChange({ target: { value: newValue } }, "customer_birthdate")}
+                                onBlur={() => handleBlur("customer_birthdate")}
                                 format="dd-MM-yyyy"
                                 inputFormat="dd-MM-yyyy"
                                 shouldDisableDate={(date) => date >= new Date()}
+                                renderInput={(params) => (
+                                    <TextField
+                                        error={Boolean(touched.customer_birthdate && errors.customer_birthdate)}
+                                        helperText={touched.customer_birthdate && errors.customer_birthdate}
+                                        fullWidth
+                                        margin="normal"
+                                    />
+                                )}
                             />
                         </LocalizationProvider>
                         <FormControl fullWidth variant="outlined" margin="normal">
@@ -202,13 +218,15 @@ export default function Customers() {
                                 value={gender}
                                 onChange={(e) => handleInputChange(e, "gender")}
                                 label="Gender"
-                                error={Boolean(errors.gender)}
+                                error={Boolean(touched.gender && errors.gender)}
+                                helperText={touched.gender && errors.gender}
+                                onBlur={() => handleBlur("gender")}
                             >
                                 {genderOptions.map((option) => (
                                     <MenuItem key={option.value} value={option.value}>
                                         {option.label}
-                                        </MenuItem>
-                                    ))}
+                                    </MenuItem>
+                                ))}
                             </Select>
                         </FormControl>
                         <TextField
@@ -218,8 +236,9 @@ export default function Customers() {
                             variant="outlined"
                             value={customer_phone_number}
                             onChange={(e) => handleInputChange(e, "customer_phone_number")}
-                            error={!!errors.customer_phone_number}
-                            helperText={errors.customer_phone_number}
+                            error={Boolean(touched.customer_phone_number && errors.customer_phone_number)}
+                            helperText={touched.customer_phone_number && errors.customer_phone_number}
+                            onBlur={() => handleBlur("customer_phone_number")}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -230,8 +249,9 @@ export default function Customers() {
                             variant="outlined"
                             value={customer_mail}
                             onChange={(e) => handleInputChange(e, "customer_mail")}
-                            error={Boolean(errors.customer_mail)}
-                            helperText={errors.customer_mail}
+                            error={Boolean(touched.customer_mail && errors.customer_mail)}
+                            helperText={touched.customer_mail && errors.customer_mail}
+                            onBlur={() => handleBlur("customer_mail")}
                         />
                         <TextField
                             fullWidth
@@ -240,8 +260,9 @@ export default function Customers() {
                             variant="outlined"
                             value={city}
                             onChange={(e) => handleInputChange(e, "city")}
-                            error={Boolean(errors.city)}
-                            helperText={errors.city}
+                            error={Boolean(touched.city && errors.city)}
+                            helperText={touched.city && errors.city}
+                            onBlur={() => handleBlur("city")}
                         />
                         <TextField
                             fullWidth
@@ -250,8 +271,9 @@ export default function Customers() {
                             variant="outlined"
                             value={address}
                             onChange={(e) => handleInputChange(e, "address")}
-                            error={Boolean(errors.address)}
-                            helperText={errors.address}
+                            error={Boolean(touched.address && errors.address)}
+                            helperText={touched.address && errors.address}
+                            onBlur={() => handleBlur("address")}
                         />
                         <TextField
                             fullWidth
@@ -260,8 +282,9 @@ export default function Customers() {
                             variant="outlined"
                             value={postal_code}
                             onChange={(e) => handleInputChange(e, "postal_code")}
-                            error={Boolean(errors.postal_code)}
-                            helperText={errors.postal_code}
+                            error={Boolean(touched.postal_code && errors.postal_code)}
+                            helperText={touched.postal_code && errors.postal_code}
+                            onBlur={() => handleBlur("postal_code")}
                         />
                     </Grid>
                 </Grid>
