85fa79f83394e1293ed7c9f1cc89bef4b4d122f2
John <john-wang@hotmail.fr>
2024-12-13 10:59:26 +0100

    .

--- episaine-front/src/constants/back.js
+++ episaine-front/src/constants/back.js
@@ -1,6 +1,6 @@
 export const BACKEND_HOST_EPISAINE = process.env.REACT_APP_BACKEND_URL;
-export const CREATE_CUSTOMER = BACKEND_HOST_EPISAINE + '/customer/add';
-export const READ_CUSTOMERS = BACKEND_HOST_EPISAINE + '/customer/get';
-export const READ_ALL_CUSTOMERS = BACKEND_HOST_EPISAINE + '/customer/get/all';
-export const UPDATE_CUSTOMER = BACKEND_HOST_EPISAINE + '/customer/update';
-export const DELETE_CUSTOMER = BACKEND_HOST_EPISAINE + '/customer/delete';
+export const CREATE_CUSTOMER = BACKEND_HOST_EPISAINE + '/customers/add';
+export const READ_CUSTOMERS = BACKEND_HOST_EPISAINE + '/customers/get';
+export const READ_ALL_CUSTOMERS = BACKEND_HOST_EPISAINE + '/customers/get/all';
+export const UPDATE_CUSTOMER = BACKEND_HOST_EPISAINE + '/customers/update';
+export const DELETE_CUSTOMER = BACKEND_HOST_EPISAINE + '/customers/delete';
