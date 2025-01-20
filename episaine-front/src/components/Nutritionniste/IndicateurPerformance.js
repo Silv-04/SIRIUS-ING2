e22a02456a7914e536f1dbcf44f550e424c32ac9
Ismail Benaissa <ismailbenaissa85@gmail.com>
2025-01-20 11:23:06 +0100

    1 - Dev fichier KPI
    Signed-off-by: Ismail Benaissa <ismailbenaissa85@gmail.com>

--- episaine-front/src/components/Nutritionniste/IndicateurPerformance.js
+++ episaine-front/src/components/Nutritionniste/IndicateurPerformance.js
@@ -8,6 +8,7 @@ import {
     Text,
     SimpleGrid,
     Icon,
+    Spacer,
 } from "@chakra-ui/react";
 import {
     FaWallet,
@@ -15,7 +16,9 @@ import {
     FaCalendarAlt,
     FaBook,
     FaCog,
+    FaChartLine,
 } from "react-icons/fa";
+import { Link as RouterLink } from "react-router-dom";
 import { FEMALE_COUNT, MALE_COUNT, TOTAL_COUNT } from "../../constants/back";
 
 export default function IndicateurPerformance() {
@@ -81,76 +84,106 @@ export default function IndicateurPerformance() {
                 p={6}
                 display="flex"
                 flexDirection="column"
-                justifyContent="space-between"
                 borderRight="1px solid #4d648d"
             >
-                <VStack align="start" spacing={8}>
+                {/* Titre EPISAINE */}
+                <Box textAlign="center" mb={8}>
                     <Heading size="md" color="#acc2ef">
                         EPISAINE
                     </Heading>
-                    <VStack align="start" spacing={4}>
-                        <Button
-                            variant="ghost"
-                            justifyContent="flex-start"
-                            leftIcon={<Icon as={FaWallet} />}
-                            _hover={{ bg: "#4d648d" }}
-                            color="white"
-                            bg="#2C3A4F"
-                            borderRadius="8px"
-                            w="100%"
-                        >
-                            Tableau de bord
-                        </Button>
-                        <Button
-                            variant="ghost"
-                            justifyContent="flex-start"
-                            leftIcon={<Icon as={FaUsers} />}
-                            _hover={{ bg: "#4d648d" }}
-                            color="white"
-                            bg="#2C3A4F"
-                            borderRadius="8px"
-                            w="100%"
-                        >
-                            Clients
-                        </Button>
-                        <Button
-                            variant="ghost"
-                            justifyContent="flex-start"
-                            leftIcon={<Icon as={FaCalendarAlt} />}
-                            _hover={{ bg: "#4d648d" }}
-                            color="white"
-                            bg="#2C3A4F"
-                            borderRadius="8px"
-                            w="100%"
-                        >
-                            Rendez-vous
-                        </Button>
-                        <Button
-                            variant="ghost"
-                            justifyContent="flex-start"
-                            leftIcon={<Icon as={FaBook} />}
-                            _hover={{ bg: "#4d648d" }}
-                            color="white"
-                            bg="#2C3A4F"
-                            borderRadius="8px"
-                            w="100%"
-                        >
-                            Bibliothèque recette
-                        </Button>
-                        <Button
-                            variant="ghost"
-                            justifyContent="flex-start"
-                            leftIcon={<Icon as={FaCog} />}
-                            _hover={{ bg: "#4d648d" }}
-                            color="white"
-                            bg="#2C3A4F"
-                            borderRadius="8px"
-                            w="100%"
-                        >
-                            Paramètre
-                        </Button>
-                    </VStack>
+                </Box>
+
+                {/* Navigation */}
+                <VStack align="start" spacing={4}>
+                    <Button
+                        as={RouterLink}
+                        to="/dashboard"
+                        variant="ghost"
+                        justifyContent="flex-start"
+                        leftIcon={<Icon as={FaWallet} />}
+                        _hover={{ bg: "#4d648d" }}
+                        color="white"
+                        bg="#2C3A4F"
+                        borderRadius="8px"
+                        w="100%"
+                    >
+                        Tableau de bord
+                    </Button>
+                    <Button
+                        as={RouterLink}
+                        to="/components/Nutritionniste/Costumer"
+                        variant="ghost"
+                        justifyContent="flex-start"
+                        leftIcon={<Icon as={FaUsers} />}
+                        _hover={{ bg: "#4d648d" }}
+                        color="white"
+                        bg="#2C3A4F"
+                        borderRadius="8px"
+                        w="100%"
+                    >
+                        Clients
+                    </Button>
+                    <Button
+                        as={RouterLink}
+                        to="/components/Nutritionniste/rdv"
+                        variant="ghost"
+                        justifyContent="flex-start"
+                        leftIcon={<Icon as={FaCalendarAlt} />}
+                        _hover={{ bg: "#4d648d" }}
+                        color="white"
+                        bg="#2C3A4F"
+                        borderRadius="8px"
+                        w="100%"
+                    >
+                        Rendez-vous
+                    </Button>
+                    <Button
+                        as={RouterLink}
+                        to="/components/Nutritionniste/recipe"
+                        variant="ghost"
+                        justifyContent="flex-start"
+                        leftIcon={<Icon as={FaBook} />}
+                        _hover={{ bg: "#4d648d" }}
+                        color="white"
+                        bg="#2C3A4F"
+                        borderRadius="8px"
+                        w="100%"
+                    >
+                        Bibliothèque recette
+                    </Button>
+                    <Button
+                        as={RouterLink}
+                        to="/IndicateurPerformance"
+                        variant="ghost"
+                        justifyContent="flex-start"
+                        leftIcon={<Icon as={FaChartLine} />}
+                        _hover={{ bg: "#4d648d" }}
+                        color="white"
+                        bg="#2C3A4F"
+                        borderRadius="8px"
+                        w="100%"
+                    >
+                        KPI
+                    </Button>
                 </VStack>
+
+                <Spacer />
+
+                <Button
+                    as={RouterLink}
+                    to="/components/Nutritionniste/Parametre"
+                    variant="ghost"
+                    justifyContent="flex-start"
+                    leftIcon={<Icon as={FaCog} />}
+                    _hover={{ bg: "#4d648d" }}
+                    color="white"
+                    bg="#2C3A4F"
+                    borderRadius="8px"
+                    w="100%"
+                    mt={4}
+                >
+                    Paramètres
+                </Button>
             </Box>
 
             {/* Main Content */}
