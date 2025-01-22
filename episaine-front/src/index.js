d1a1cdbd1ca2b19f7f3d031df4c7b77583f624eb
Wang John <john-wang@hotmail.fr>
2025-01-22 09:26:02 +0100

    router settings fixes

--- episaine-front/src/index.js
+++ episaine-front/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, extendTheme, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from './pages/App';
 
-const theme = extendTheme({
-    config: {
-        initialColorMode: "light",
-        useSystemColorMode: false,
-    },
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <ChakraProvider theme={theme}>
-            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ChakraProvider >
             <App />
         </ChakraProvider>
     </React.StrictMode>
--- episaine-front/src/pages/App.jsx
+++ episaine-front/src/pages/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import HomeNutritionniste from "./nutritionniste/HomeNutritionniste";
 import IndicateurPerformance from "./nutritionniste/IndicateurPerformance";
 import CustomerMainPage from "./customers/CustomerMainPage";
@@ -17,33 +16,29 @@ import Recipe from "./nutritionniste/recipe"
 import Customer from "./nutritionniste/Customer"
 import ParametreNutritionist from "../components/nutritionist/Parametre"
 
-const theme = createTheme();
-
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
+        <Router basename="/episaine/">
+            <Routes>
+                <Route path="/" element={<Home />} />
 
-                    <Route path="/client/" element={<CustomerMainPage />} />
-                    <Route path="/client/recettes/" element={<ExistingCustomer />} />
-                    <Route path="/client/recettes/informations/" element={<InformationsPage />} />
-                    <Route path="/client/recettes/informations/choix/" element={<RecipesList />} />
-                    <Route path="/client/recettes/informations/choix/resultat/" element={<Result />} />
-                    <Route path="/client/recettes/creation_profil/" element={<CreateCustomers />} />
-                    <Route path="/client/parametre/" element={<CustomerParameter />} />
+                <Route path="/client/" element={<CustomerMainPage />} />
+                <Route path="/client/recettes/" element={<ExistingCustomer />} />
+                <Route path="/client/recettes/informations/" element={<InformationsPage />} />
+                <Route path="/client/recettes/informations/choix/" element={<RecipesList />} />
+                <Route path="/client/recettes/informations/choix/resultat/" element={<Result />} />
+                <Route path="/client/recettes/creation_profil/" element={<CreateCustomers />} />
+                <Route path="/client/parametre/" element={<CustomerParameter />} />
 
-                    <Route path="/nutritionniste/" element={<HomeNutritionniste />} />
-                    <Route path="/indicateurPerformance/" element={<IndicateurPerformance />} />
-                    <Route path="/dashboard/" element={<Dashboard />} />
-                    <Route path="/components/nutritionniste/customer/" element={<Customer />} />
-                    <Route path="/components/nutritionniste/rdv/" element={<Rdv />} />
-                    <Route path="/components/nutritionniste/recipe/" element={<Recipe />} />
-                    <Route path="/components/nutritionniste/Parametre/" element={<ParametreNutritionist />} />
-                </Routes>
-            </Router>
-        </ThemeProvider>
+                <Route path="/nutritionniste/" element={<HomeNutritionniste />} />
+                <Route path="/indicateurPerformance/" element={<IndicateurPerformance />} />
+                <Route path="/dashboard/" element={<Dashboard />} />
+                <Route path="/components/nutritionniste/customer/" element={<Customer />} />
+                <Route path="/components/nutritionniste/rdv/" element={<Rdv />} />
+                <Route path="/components/nutritionniste/recipe/" element={<Recipe />} />
+                <Route path="/components/nutritionniste/Parametre/" element={<ParametreNutritionist />} />
+            </Routes>
+        </Router>
     );
 }
 
