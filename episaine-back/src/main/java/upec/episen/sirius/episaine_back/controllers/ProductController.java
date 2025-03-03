fe105406adae8a1ab9e35879f2a6c97fd5657cca
Ismail Benaissa <ismailbenaissa85@gmail.com>
2025-03-03 11:44:54 +0100

    Created ProductController with Javadoc comments.
    Signed-off-by: Ismail Benaissa <ismailbenaissa85@gmail.com>

--- episaine-back/src/main/java/upec/episen/sirius/episaine_back/controllers/ProductController.java
+++ episaine-back/src/main/java/upec/episen/sirius/episaine_back/controllers/ProductController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import upec.episen.sirius.episaine_back.models.Product;
 import upec.episen.sirius.episaine_back.services.ProductService;
-
-
 import java.util.List;
+
+/**
+ * Controller class for managing product-related HTTP requests.
+ * Handles API  for retrieving all products, filtering by category, and getting product by ID.
+ */
 
 @RestController
 @RequestMapping("/products")
@@ -16,16 +19,30 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    /**
+     * Retrieves all products from the database.
+     * @return A list of all available products.
+     */
     @GetMapping("/all")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
+    /**
+     * Retrieves products by their category.
+     * @param category The category to filter products by.
+     * @return A list of products that belong to the specified category.
+     */
     @GetMapping("/category")
     public List<Product> getProductsByCategory(@RequestParam String category) {
         return productService.getProductsByCategory(category);
     }
 
+    /**
+     * Retrieves a specific product by its ID.
+     * @param id The ID of the product to retrieve.
+     * @return The product with the specified ID or null if not found.
+     */
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable Long id) {
         return productService.getProductById(id);
