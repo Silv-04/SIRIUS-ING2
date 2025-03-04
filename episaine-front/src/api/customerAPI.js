72316290d19ed06b7b9928c73d59aa381a85d7dc
Wang John <john-wang@hotmail.fr>
2025-03-04 17:28:14 +0100

    Resolved merge error

--- episaine-front/src/api/customerAPI.js
+++ episaine-front/src/api/customerAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CREATE_INFORMATIONS, GET_INFORMATIONS_BY_CUSTOMER_ID, UPDATE_INFORMATIONS } from '../constants/back';
+import { CREATE_INFORMATIONS, GET_INFORMATIONS_BY_CUSTOMER_ID, UPDATE_INFORMATIONS, GET_PROJECTION_VALUES } from '../constants/back';
 
 export async function updateCustomerInformation(information) {
     return await axios.post(UPDATE_INFORMATIONS, information);
@@ -11,4 +11,8 @@ export async function createCustomerInformation(information) {
 
 export async function getCustomerInformationByCustomerId(customerId) {
     return await axios.get(GET_INFORMATIONS_BY_CUSTOMER_ID + "/" + customerId);
+}
+
+export async function projection(height, age, gender, number_of_meals, nbOfDays, weight, recipesList) {
+    return await axios.get(GET_PROJECTION_VALUES + `?height=${height}&age=${age}&gender&number_of_meals=${number_of_meals}&nbOfDays=${nbOfDays}&weight=${weight}&recipesList=${recipesList}`);
 }
--- episaine-front/src/pages/customers/ExistingCustomer.jsx
+++ episaine-front/src/pages/customers/ExistingCustomer.jsx
@@ -46,18 +46,20 @@ function ExistingCustomerOrNot() {
                         <Divider orientation="vertical" />
                     </GridItem>
                     <GridItem>
-                        <Text fontWeight={"bold"} textAlign={"center"}>Client existant</Text>
-                        <Input
-                            placeholder="Numéro client"
-                            value={customerNumber}
-                            onChange={handleChange}
-                            fullWidth
-                            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                        />
-                        <Grid align={"center"} templateColumns={"repeat(2, 1fr"}>
-                            <GridItem gridColumn={1}><Button bgColor={"#bbf7d0"} onClick={handleValidate}>Valider</Button></GridItem>
-                            <GridItem gridColumn={2}><Button bgColor={"#fca5a5"} onClick={handleReset}>Annuler</Button></GridItem>
-                        </Grid>
+                        <form onSubmit={handleValidate}>
+                            <Text fontWeight={"bold"} textAlign={"center"}>Client existant</Text>
+                            <Input
+                                placeholder="Numéro client"
+                                value={customerNumber}
+                                onChange={handleChange}
+                                fullWidth
+                                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                            />
+                            <Grid align={"center"} templateColumns={"repeat(2, 1fr"}>
+                                <GridItem gridColumn={1}><Button bgColor={"#bbf7d0"} type={"submit"}>Valider</Button></GridItem>
+                                <GridItem gridColumn={2}><Button bgColor={"#fca5a5"} onClick={handleReset}>Annuler</Button></GridItem>
+                            </Grid>
+                        </form>
                     </GridItem>
                 </Grid>
             </Grid>
