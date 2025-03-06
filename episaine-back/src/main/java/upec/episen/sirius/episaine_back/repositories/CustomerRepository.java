8998865d604bfbde3fb003b2183b169533efcd52
Ismail Benaissa <ismailbenaissa85@gmail.com>
2025-03-06 09:24:22 +0100

    Suppression de RecipesController.java

--- episaine-back/src/main/java/upec/episen/sirius/episaine_back/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package upec.episen.sirius.episaine_back.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import upec.episen.sirius.episaine_back.models.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    
-}
