66726d36f9283574cfe3105f1c993d9849368b2b
Ismail Benaissa <ismailbenaissa85@gmail.com>
2025-03-19 12:26:28 +0100

    State for nutrients
    Signed-off-by: Ismail Benaissa <ismailbenaissa85@gmail.com>

--- episaine-front/src/pages/nutritionniste/recipe.jsx
+++ episaine-front/src/pages/nutritionniste/recipe.jsx
@@ -17,4 +17,32 @@ export default function Recipe() {
     const [minCalories, setMinCalories] = useState(0);
     const [maxCalories, setMaxCalories] = useState(1000);
     const [error, setError] = useState("");
-}
+
+    // State for nutrients
+
+    const [minGlucides, setMinGlucides] = useState(null);
+    const [maxGlucides, setMaxGlucides] = useState(null);
+    const [minLipides, setMinLipides] = useState(null);
+    const [maxLipides, setMaxLipides] = useState(null);
+    const [minGlucose, setMinGlucose] = useState(null);
+    const [maxGlucose, setMaxGlucose] = useState(null);
+    const [minLactose, setMinLactose] = useState(null);
+    const [maxLactose, setMaxLactose] =useState(null);
+    const [minMaltose, setMinMaltose] = useState(null);
+    const [maxMaltose, setMaxMaltose] = useState(null);
+    const [minAmidon, setMinAmidon] = useState(null);
+    const [maxAmidon, setMaxAmidon] = useState(null);
+    const [minFibres, setMinFibres] = useState(null);
+    const [maxFibres, setMaxFibres] = useState(null);
+    const [minCholesterol, setMinCholesterol] = useState(null);
+    const [maxCholesterol, setMaxCholesterol] = useState(null);
+    const [minSel, setMinSel] = useState(null);
+    const [maxSel, setMaxSel] = useState(null);
+    const [minCalcium, setMinCalcium] = useState(null);
+    const [maxCalcium, setMaxCalcium] = useState(null);
+    const [minCuivre, setMinCuivre] = useState(null);
+    const [maxCuivre, setMaxCuivre] = useState(null);
+    const [minFer, setMinFer] = useState(null);
+    const [maxFer, setMaxFer] = useState(null);
+    const [minProteines625, setMinProteines625] = useState(null);
+    const [maxProteines625, setMaxProteines625] = useState(null);}
