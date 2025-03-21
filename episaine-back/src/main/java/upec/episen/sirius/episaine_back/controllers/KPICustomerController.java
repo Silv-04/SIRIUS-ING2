5354d8974c5246c5cf80270f90d574408ba91fec
Ismail Benaissa <ismailbenaissa85@gmail.com>
2025-03-21 11:19:53 +0100

    Adding file KPICustomerController
    Signed-off-by: Ismail Benaissa <ismailbenaissa85@gmail.com>

---
+++ episaine-back/src/main/java/upec/episen/sirius/episaine_back/controllers/KPICustomerController.java
@@ -0,0 +1,10 @@
+package upec.episen.sirius.episaine_back.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/kpis/customers")
+public class KPICustomerController {
+
+}
