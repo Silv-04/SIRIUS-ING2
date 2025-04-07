8f6509860f8f1975defd47a4c7b02ed9ba5527a3
Ismail Benaissa <ismailbenaissa85@gmail.com>
2025-04-07 10:41:37 +0200

    docs(getFcmResults): Implement FCM & document clustering logic and point extraction process

--- episaine-back/src/main/java/upec/episen/sirius/episaine_back/services/KPICustomerService.java
+++ episaine-back/src/main/java/upec/episen/sirius/episaine_back/services/KPICustomerService.java
@@ -2,7 +2,14 @@ package upec.episen.sirius.episaine_back.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.commons.math3.ml.clustering.CentroidCluster;
+
 import upec.episen.sirius.episaine_back.repositories.KPICustomerRepository;
+
+import org.apache.commons.math3.ml.clustering.DoublePoint;
+import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer;
+
+import org.apache.commons.math3.linear.*;
 
 import java.util.*;
 
@@ -43,6 +50,7 @@ public class KPICustomerService {
      *
      * @return the total number of customers
      */
+
     public long getTotalCount() {
         return kpiCustomerRepository.countTotalCustomers();
     }
@@ -153,6 +161,125 @@ public class KPICustomerService {
         return stats;
     }
 
+    /**
+     * Effectuer l'ACM sur toutes les tranches d'âge et les données calculées (IMC, poids, taille).
+     * @return les résultats de l'ACM sous forme de liste.
+     */
+
+    public List<Map<String, Object>> getAcmResults() {
+        List<Map<String, Object>> stats = getStatsByAgeGroup();
+
+        double[][] matrixData = new double[stats.size()][3];
+        for (int i = 0; i < stats.size(); i++) {
+            Map<String, Object> stat = stats.get(i);
+            matrixData[i][0] = ((Number) stat.get("imc")).doubleValue();
+            matrixData[i][1] = ((Number) stat.get("averageWeight")).doubleValue();
+            matrixData[i][2] = ((Number) stat.get("averageHeight")).doubleValue();
+        }
+
+        RealMatrix matrix = new Array2DRowRealMatrix(matrixData);
+        SingularValueDecomposition svd = new SingularValueDecomposition(matrix);
+        RealMatrix U = svd.getU();
+        RealMatrix V = svd.getV();
+
+        List<Map<String, Object>> acmResults = new ArrayList<>();
+
+        for (int i = 0; i < U.getRowDimension(); i++) {
+            Map<String, Object> row = new HashMap<>();
+            row.put("ageGroup", stats.get(i).get("ageGroup"));
+            row.put("U", U.getRow(i));
+            acmResults.add(row);
+        }
+
+        Map<String, Object> vData = new HashMap<>();
+        double[][] vMatrixData = V.getData();
+        vData.put("V", vMatrixData);
+        acmResults.add(vData);
+
+        return acmResults;
+    }
+
+    /**
+     * Perform FCM (Fuzzy C-Means) on BMI, Weight and Height data.
+     * @return FCM results in list form.
+     */
+
+    public List<Map<String, Object>> getFcmResults() {
+        /**
+         * Retrieve statistics containing average weight and height per age group
+         */
+        List<Map<String, Object>> stats = getStatsByAgeGroup();
+        List<DoublePoint> points = new ArrayList<>();
+
+        /**
+         * Convert each valid weight/height pair into a DoublePoint for clustering
+         */
+        for (Map<String, Object> stat : stats) {
+            Object weightObj = stat.get("averageWeight");
+            Object heightObj = stat.get("averageHeight");
+
+        /**
+         * Only process if both values are numeric
+         */
+
+            if (weightObj instanceof Number && heightObj instanceof Number) {
+                double avgWeight = ((Number) weightObj).doubleValue();
+                double avgHeight = ((Number) heightObj).doubleValue();
+
+                /**
+                 * Create a 2D point [weight, height] and add it to the list
+                 */
+                points.add(new DoublePoint(new double[]{avgWeight, avgHeight}));
+            }
+        }
+        /**
+         * Return an empty result if no valid points were found
+         */
+        if (points.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        /**
+         * Initialize the KMeans++ clusterer with 3 clusters and 1000 maximum iterations
+         */
+
+        KMeansPlusPlusClusterer<DoublePoint> clusterer = new KMeansPlusPlusClusterer<>(3, 1000);
+
+        /**
+         *  Perform the clustering algorithm on the list of data points
+         */
+
+        List<CentroidCluster<DoublePoint>> clusters = clusterer.cluster(points);
 
 
+        List<Map<String, Object>> fcmResults = new ArrayList<>();
+        int clusterId = 1;
+
+        /**
+         * Iterate over each cluster to extract cluster center and assigned points
+         */
+
+        for (CentroidCluster<DoublePoint> cluster : clusters) {
+            Map<String, Object> clusterData = new HashMap<>();
+            clusterData.put("clusterId", clusterId++);
+            clusterData.put("center", cluster.getCenter().getPoint());
+
+            /**
+             * Extract the coordinates of all points belonging to this cluster
+             */
+
+            List<double[]> clusterPoints = cluster.getPoints().stream()
+                    .map(DoublePoint::getPoint)
+                    .toList();
+            clusterData.put("points", clusterPoints);
+
+            fcmResults.add(clusterData);
+        }
+
+        /**
+         * Return the list of clusters with their IDs, centers, and data points
+         */
+
+        return fcmResults;
+    }
 }
